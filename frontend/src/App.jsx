b4617d45c48e70538bccf19bbd41b9772799b184
ABHIRAM KRISHNA S <abhiramkrishna085@gmail.com>
2023-11-04 11:24:43 +0530

    First Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@material-tailwind/react";
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Demo</h1>
+      <Button>Hi</Button>
+    </>
+  )
+}
+
+export default App
